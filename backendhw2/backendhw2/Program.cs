4de18d4668bb45f4b0eadfbd0a9837c83db5ef56
RichFisher <rich.fishernc@gmail.com>
2019-03-01 11:16:49 -0600

    second homework works

---
+++ backendhw2/backendhw2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace backendhw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var output = "";
+            var splitInput = input.Split(",");
+            var counter = 0;
+            var total = 1;
+            Console.WriteLine("Would you like to multiply or square? Enter M for multiply or S for square.");
+            var type = Console.ReadLine();
+            if (type == "M")
+            {
+                foreach (string num in splitInput)
+                {
+                    var curNum = Int32.Parse(num);
+                    total = total * curNum;
+                }
+                output += total;
+            } else if (type == "S")
+            {
+                foreach (string num in splitInput)
+                {
+                    counter++;
+                    var curNum = Int32.Parse(num);
+                    curNum = curNum * curNum;
+                    output += curNum;
+                    if (counter < splitInput.Length)
+                    {
+                        output += ",";
+                    }
+                }
+            }           
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
