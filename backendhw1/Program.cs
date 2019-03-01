ae178d0e96d8ea0d9c674f650f054682ccf3cbc5
RichFisher <rich.fishernc@gmail.com>
2019-02-28 19:54:47 -0600

    it works

---
+++ backendhw1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace backendhw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var counter = 0;
+            string output = "";
+            foreach(char item in input) {
+                counter++;
+                for(var i = 1; i <= counter; i++)
+                {
+                    if (i == 1)
+                    {
+                        output += char.ToUpper(item);
+                    } else
+                    {
+                        output += item;
+                    }
+                    if (i == counter && counter < input.Length)
+                    {
+                        output += "-";
+                    }
+                }
+            }
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
